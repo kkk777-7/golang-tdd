ba7a93f721f76e9460fddebc6975c5fa3894fb0d
github kkk777-7 <kota.kimura0725@gmail.com>
2022-01-15 20:03:56 +0900

    Updated Hello Test

--- hello.go
+++ hello.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
