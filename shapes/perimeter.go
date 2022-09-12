fcd37a0c7fb2de7d956adde29cabed2aaee9392d
github kkk777-7 <kota.kimura0725@gmail.com>
2022-09-13 00:32:58 +0900

    update table driven test for shapes

--- shapes/perimeter.go
+++ shapes/perimeter.go
@@ -15,6 +15,11 @@ type Circle struct {
 	radius float64
 }
 
+type Triangle struct {
+	base   float64
+	height float64
+}
+
 func (r *Rectangle) Perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
@@ -25,4 +30,8 @@ func (r *Rectangle) Area() float64 {
 
 func (c *Circle) Area() float64 {
 	return c.radius * c.radius * math.Pi
+}
+
+func (t *Triangle) Area() float64 {
+	return t.base * t.height / 2.0
 }
--- shapes/perimeter_test.go
+++ shapes/perimeter_test.go
@@ -13,20 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %f want %f", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: &Rectangle{10.0, 10.0}, want: 100.0},
+		{name: "Circle", shape: &Circle{10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: &Triangle{10.0, 10.0}, want: 50.0},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := &Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
 
-	t.Run("circles", func(t *testing.T) {
-		circle := &Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 }
