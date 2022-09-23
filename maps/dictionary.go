121b562b0aa5359c5ef6573e9c3b852c6596a5d5
github kkk777-7 <kota.kimura0725@gmail.com>
2022-09-23 18:25:16 +0900

    Add function

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -12,4 +15,13 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(key, word string) error {
+	_, ok := d[key]
+	if ok {
+		return ErrWordExists
+	}
+	d[key] = word
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,34 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		err := dictionary.Add("test", "this is just a test")
+
+		assertError(t, err, nil)
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
+		if err != nil {
+			t.Fatal("should find added word:", err)
+		}
+		assertStrings(t, got, want)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+		err := dictionary.Add("test", "new test")
+
+		assertError(t, err, ErrWordExists)
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
+		if err != nil {
+			t.Fatal("should find added word:", err)
+		}
+		assertStrings(t, got, want)
+	})
+}
+
 func assertStrings(t *testing.T, got string, want string) {
 	t.Helper()
 	if got != want {
@@ -33,5 +61,11 @@ func assertError(t *testing.T, got error, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
+	}
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
 	}
 }
