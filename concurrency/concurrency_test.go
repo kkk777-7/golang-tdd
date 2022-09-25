2e3fea8a3287a449fd23e282c18f9026960abc04
github kkk777-7 <kota.kimura0725@gmail.com>
2022-09-25 10:53:20 +0900

    Add benchmark

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,12 +3,18 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	if url == "waat://furhurterwe.geds" {
 		return false
 	}
+	return true
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
 	return true
 }
 
@@ -29,5 +35,15 @@ func TestCheckWebsite(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("want %v, got %v", want, got)
+	}
+}
+
+func BechmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
